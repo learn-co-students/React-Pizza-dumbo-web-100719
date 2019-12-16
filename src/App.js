a85dd9cf1898b333da593556c8a177f197cfe469
Diego Martinez <55361667+dmartinezm@users.noreply.github.com>
2019-12-16 15:58:21 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React, { Component, Fragment } from 'react';
-import Header from './components/Header'
-import PizzaForm from './components/PizzaForm'
-import PizzaList from './containers/PizzaList'
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+import PizzaForm from "./components/PizzaForm";
+import PizzaList from "./containers/PizzaList";
 class App extends Component {
+  state = {
+    pizzas: [],
+    editPizza: {}
+  };
+
+  // changePizzaState = (arg) => {
+  //   this.setState({
+  //     pizzas: arg
+  //   })
+  // }
+  // use map to mutate
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/pizzas`)
+      .then(resp => resp.json())
+      // .then(pizzasArray => {this.changePizzaState(pizzasArray)} );
+      .then(pizzasArray => {
+        this.setState({
+          pizzas: pizzasArray
+        });
+      });
+  }
+
+  editPizzaObj = pizzaObj => {
+    // console.log(pizzaObj);
+    this.setState({
+      editPizza: pizzaObj
+    });
+  };
+
+  submitPizzaObj = event => {
+    console.log({
+      ...this.state.editPizza,
+      [event.target.name]: event.target.value
+    });
+    console.log(event.target.name, event.target.value);
+  };
+
   render() {
+    console.log(this.state.editPizza);
     return (
       <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <Header />
+        <PizzaForm
+          pizza={this.state.editPizza}
+          handleOnChange={this.submitPizzaObj}
+        />
+        <PizzaList
+          pizzas={this.state.pizzas}
+          editPizzaObj={this.editPizzaObj}
+        />
       </Fragment>
     );
   }
